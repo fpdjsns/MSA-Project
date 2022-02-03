46f7c2c91e20aa89c3722a0c75f79cd275fbf45d
배현수 <withham1@naver.com>
2022-02-03 21:22:56 +0900

    #1 init starterProject

---
+++ starterProject/src/main/kotlin/com/withhamit/starterProject/StarterProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.withhamit.starterProject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarterProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarterProjectApplication>(*args)
+}
---
+++ starterProject/src/test/kotlin/com/withhamit/starterProject/StarterProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.withhamit.starterProject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarterProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
